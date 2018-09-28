5bb4e16c1a81205455cad7a08232ca66d3928e41
Jeremy Greer <jex.grizzle@gmail.com>
2018-09-28 15:55:07 -0400

    First pass at running tests as sync spawned process

--- index.js
+++ index.js
@@ -1,1 +1,89 @@
-console.log('yo')
+const fs = require('fs')
+const path = require('path')
+const spawn = require('child_process').spawnSync
+
+function log () {
+  if (process.env.DEBUG) {
+    const args = Array.prototype.slice.apply(arguments)
+    console.log.apply(console.log, args)
+  }
+}
+
+const cwd = process.cwd()
+
+// find solution file
+const solution = process.argv[2]
+const solutionPath = path.resolve(cwd, solution)
+// log('solutionPath', solutionPath)
+// TODO: handle missing
+
+// find test dir
+const testDir = process.argv[3]
+const testDirPath = path.resolve(cwd, testDir)
+// log('testDirPath', testDirPath)
+// TODO: handle missing
+
+const outputPath = path.resolve(testDir, 'out.txt')
+log('outputPath', outputPath)
+
+// find inputs
+const inputRegex = (/^input(\d+)\.txt$/)
+const inputsDir = path.resolve(testDirPath, 'input')
+const inputs = fs.readdirSync(inputsDir)
+  .filter(x => inputRegex.test(x))
+  .map(x => path.resolve(inputsDir, x))
+// log('inputs', inputs)
+
+// for each input, find output
+const outputsDir = path.resolve(testDirPath, 'output')
+const outputs = inputs.map(x => {
+  const name = path.basename(x)
+  const num = inputRegex.exec(name)[1]
+  return path.resolve(outputsDir, `output${num}.txt`)
+})
+// log('outputs', outputs)
+
+// run tests
+// process.env.OUTPUT_PATH
+const runTest = (solution, input, output) => {
+  // log('runTest', solution)
+  // cat tests/input/input00.txt | env OUTPUT_PATH=(pwd)/out.txt node solution.js
+
+  const args = [
+    solution,
+  ]
+
+  const inputData = fs.readFileSync(input, 'utf8')
+  // log('inputData', inputData)
+
+  const options = {
+    input: inputData,
+    env: Object.assign({}, process.env, {
+      OUTPUT_PATH: outputPath,
+    }),
+  }
+
+  const result = spawn('node', args, options)
+  log('status', result.status)
+  log('stderr', result.stderr)
+  log('error', result.error)
+  // TODO: check error result
+
+  // If everything was fine, compare result to expected.
+  // TODO: cache input/output files
+  const expected = fs.readFileSync(output, 'utf8').trim()
+  const actual = fs.readFileSync(outputPath, 'utf8').trim()
+
+  if (expected !== actual) {
+    throw new Error(`expected ${actual} to be ${expected}`)
+  }
+}
+
+inputs.forEach((x, i) => {
+  try {
+    runTest(solutionPath, x, outputs[i])
+  } catch (error) {
+    console.log(`failed ${x}`)
+    console.log(error)
+  }
+})
